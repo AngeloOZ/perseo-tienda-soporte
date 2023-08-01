c9df7dcb2bb89bc368ea9d450846a03afcea7ca8
angelooz <angelo-mjz7@hotmail.com>
2023-08-01 14:47:25 -0500

    fix

--- routes/soporte.php
+++ routes/soporte.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\SoporteController;
 use App\Http\Controllers\SoporteEspcialController;
 use App\Http\Controllers\TicketSoporteController;
 use App\Http\Controllers\WhatsappController;
+use App\Models\SoporteEspecial;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('soporte')->group(function () {
@@ -13,57 +15,71 @@ Route::prefix('soporte')->group(function () {
     Route::get('/consultar-estado/{ruc}', [TicketSoporteController::class, 'validar_ticket_activo'])->name("soporte.consultar_estado");
     Route::get('/resultado-registro-ticket/{numero?}', [TicketSoporteController::class, 'resultado_registro'])->name("soporte.resultado_registro");
     Route::post('/crear-ticket', [TicketSoporteController::class, 'crear_ticket'])->name("soporte.crear_ticket");
-    Route::get('/calificar-ticket/{ticket}', [TicketSoporteController::class, 'calificar_soporte_vista'])->name('soporte.calificar_ticket');
-    Route::post('/calificar-ticket', [TicketSoporteController::class, 'registrar_calificacion_soporte'])->name('soporte.registrar_califcacion');
 
-    Route::get('/login', [SoporteController::class, 'render_login'])->name('soporte.auth.login');
+    Route::get('/calificar-soporte/{ticket}', [TicketSoporteController::class, 'calificar_soporte_vista'])->name('soporte.calificar_ticket');
+
+    Route::post('/calificar-soporte', [TicketSoporteController::class, 'registrar_calificacion_soporte'])->name('soporte.registrar_califcacion');
+
+    /* Login */
+    Route::get('/login',  [SoporteController::class, 'render_login'])->name('soporte.auth.login');
     Route::post('/login', [SoporteController::class, 'login_soporte'])->name('soporte.login');
 
 
     Route::middleware(['auth'])->group(function () {
 
         Route::get('redirect-index-by-rol', [SoporteController::class, 'redirect_by_rol'])->name("redirect.rol");
+
         Route::post('enviar-correo-cliente', [TicketSoporteController::class, 'enviar_correo_cliente'])->name('soporte.enviar_correo_cliente');
         Route::get('/editar-ticket/{ticket}', [TicketSoporteController::class, 'editar_ticket'])->name('soporte.editar');
         Route::put('/actualizar-ticket/{ticket}', [TicketSoporteController::class, 'actualizar_estado_ticket'])->name('soporte.actualizar_estado_ticket');
 
-        Route::prefix('tecnico')->group(function(){
+
+        Route::prefix('tecnico')->group(function () {
             Route::get('/listado-tickets-activos', [TicketSoporteController::class, 'listado_de_tickets_activos'])->name("soporte.listado.activos");
+
             Route::get('/listado-tickets-desarrollo', [TicketSoporteController::class, 'listado_de_tickets_desarrollo'])->name("soporte.listado.desarrollo");
             Route::post('/filtrado/listado-desarollo', [TicketSoporteController::class, 'filtrado_listado_de_tickets_desarrollo'])->name("soporte.filtrado.listado_desarrollo");
+
             Route::get('/listado-tickets-cerrados', [TicketSoporteController::class, 'listado_de_tickets_cerrados'])->name("soporte.listado.cerrados");
             Route::get('/cambiar-estado-disponibilidad/{id_user?}', [TicketSoporteController::class, 'cambiar_disponibilidad'])->name('soporte.cambiar.disponibilidad');
-    
+
             Route::get('/mis-calificaciones', [TicketSoporteController::class, 'ver_resporte_calificacione_tecnico'])->name("soporte.mis_calificaciones");
             Route::post('/filtrado-mis-calificaciones', [TicketSoporteController::class, 'filtrado_reporte_calificaciones_tecnico'])->name("soporte.mis_calificaciones_filtrado");
-    
-            Route::get('/ver-calificaciones', [TicketSoporteController::class, 'ver_calificaciones_tecnicos'])->name('soporte.ver.calificaciones.tecnicos');
+
+            Route::get('/ver-calificaciones-tecnicos', [TicketSoporteController::class, 'ver_calificaciones_tecnicos'])->name('soporte.ver.calificaciones.tecnicos');
             Route::get('/reactivar-encuesta/{ticket}', [TicketSoporteController::class, 'reactivar_ecuesta'])->name('soporte.reactivar_ecuesta');
 
-
+            Route::get('/listado-soportes-especiales', [SoporteEspcialController::class, 'listar_soporte_especial'])->name('sop.listar_soporte_especial');
+            Route::post('/filtrado-soportes-especiales', [SoporteEspcialController::class, 'filtrado_soporte_especial_tecnico'])->name('soporte.filtrado.tec');
+            Route::get('/agregar-soportes-especiales', [SoporteEspcialController::class, 'agregar_soporte_especial'])->name('sop.agregar_soporte_especial');
+            Route::get('/editar-soportes-especiales/{soporte}', [SoporteEspcialController::class, 'editar_soporte_especial'])->name('sop.editar_soporte_especial');
+            Route::post('/registrar-soportes-especiales', [SoporteEspcialController::class, 'registrar_soporte_especial'])->name('sop.registrar_soporte_especial');
+            Route::put('/actualizar-soportes-especiales/{soporte}', [SoporteEspcialController::class, 'actualizar_soporte_especial'])->name('sop.actualizar_soporte_especial');
+            Route::post('/registrar-actividad/{soporte}', [SoporteEspcialController::class, 'registrar_actividad_soporte'])->name('sop.registrar_actividad_soporte');
         });
 
-        Route::prefix('desarrollo')->group(function(){
+
+        Route::prefix('desarrollo')->group(function () {
             Route::get('/listado-tickets-revisor', [TicketSoporteController::class, 'listado_de_tickets_desarrollo_revisor'])->name("soporte.listado.revidor.desarrollo");
 
             Route::get('/editar-ticket/{ticket}', [TicketSoporteController::class, 'editar_ticket_desarrollo'])->name('soporte.editar.desarrollo');
         });
-        
-        Route::prefix('revisor')->group(function(){
-            Route::get('/listado-tickets', [TicketSoporteController::class, 'listado_tickets_revisor'])->name('soporte.listado.revisor');
-            Route::get('/editar-ticket/{ticket}', [TicketSoporteController::class, 'editar_ticket_revisor'])->name('soporte.editar.revisor');
-            Route::get('/estado-de-tecnicos', [TicketSoporteController::class, 'listado_estado_tecnicos'])->name('soporte.listado.estado_tecnicos');
-            Route::post('/listado-tickets', [TicketSoporteController::class, 'filtrado_tickets_revisor'])->name('soporte.listado_filtrado.revisor');
-            Route::delete('/eliminar-soporte/{ticket}', [TicketSoporteController::class, 'eliminar_soporte_revisor'])->name('soporte.eliminar_ticket');
-    
-            
-            Route::prefix('reporte')->group(function(){
-                Route::get('/tickets-tecnicos', [TicketSoporteController::class, 'ver_resporte_soportes'])->name('soporte.reporte_soporte');
-                Route::post('/tickets-tecnicos', [TicketSoporteController::class, 'filtrado_reporte_soporte'])->name('soporte.filtrado_reporte_soporte');
 
-                Route::get('/calificaciones-tecnicos', [TicketSoporteController::class, 'ver_resporte_calificaciones'])->name('soporte.reporte_calificaicones');
-                Route::post('/calificaciones-tecnicos', [TicketSoporteController::class, 'filtrado_reporte_calificaciones'])->name('soporte.filtrado_reporte_calificaicones');
-            });
+        Route::prefix('revisor')->group(function () {
+            Route::get('/listado-tickets-revisor', [TicketSoporteController::class, 'listado_tickets_revisor'])->name('soporte.listado.revisor');
+            Route::post('/listado-tickets-revisor', [TicketSoporteController::class, 'filtrado_tickets_revisor'])->name('soporte.listado_filtrado.revisor');
+            Route::get('/editar-ticket-revisor/{ticket}', [TicketSoporteController::class, 'editar_ticket_revisor'])->name('soporte.editar.revisor');
+            Route::get('/estado-de-tecnicos', [TicketSoporteController::class, 'listado_estado_tecnicos'])->name('soporte.listado.estado_tecnicos');
+            Route::delete('/eliminar-soporte-revisor/{ticket}', [TicketSoporteController::class, 'eliminar_soporte_revisor'])->name('soporte.eliminar_ticket');
+
+            Route::get('/reporte-tickets-tecnicos', [TicketSoporteController::class, 'ver_resporte_soportes'])->name('soporte.reporte_soporte');
+            Route::post('/reporte-tickets-tecnicos', [TicketSoporteController::class, 'filtrado_reporte_soporte'])->name('soporte.filtrado_reporte_soporte');
+
+            Route::get('/reporte-calificaciones-tecnicos', [TicketSoporteController::class, 'ver_resporte_calificaciones'])->name('soporte.reporte_calificaicones');
+            Route::post('/reporte-calificaciones-tecnicos', [TicketSoporteController::class, 'filtrado_reporte_calificaciones'])->name('soporte.filtrado_reporte_calificaicones');
+
+            Route::get('/listado-revisor-soportes-especial', [SoporteEspcialController::class, 'revisor_listar_soporte_especial'])->name('soporte.revisor_listar_soporte_especial');
+            Route::post('/filtrado-revisor-soportes-especial', [SoporteEspcialController::class, 'filtrado_soporte_especial'])->name('soporte.filtrado_soporte_especial');
         });
 
         Route::prefix('calificaciones')->group(function () {
@@ -79,7 +95,6 @@ Route::prefix('soporte')->group(function () {
 
         Route::prefix('whatsapp')->group(function () {
             Route::get('/configuracion', [WhatsappController::class, 'index'])->name('config.whatsapp');
-
             Route::post('/iniciar', [WhatsappController::class, 'iniciar_whatsapp'])->name('whatsapp.iniciar');
             Route::post('/obtener-qr', [WhatsappController::class, 'obtener_qr_whatsapp'])->name('whatsapp.obtener.qr');
             Route::post('/cerrar', [WhatsappController::class, 'cerrar_whatsapp'])->name('whatsapp.cerrar');
@@ -88,23 +103,11 @@ Route::prefix('soporte')->group(function () {
         });
 
         Route::prefix('soporte-especial')->group(function () {
-            /* Registro desde la tienda */
-            Route::post('/registrar-ventas/{factura}', [SoporteEspcialController::class, 'registrar_capacitacion_ventas'])->name('soporte.registrar_capacitacion_ventas');
+            /* tienda */
+            Route::post('/registrar-capacitacion-ventas/{factura}', [SoporteEspcialController::class, 'registrar_capacitacion_ventas'])->name('soporte.registrar_capacitacion_ventas');
 
-            /* Tecnico */
-            Route::get('/listado', [SoporteEspcialController::class, 'listar_soporte_especial'])->name('sop.listar_soporte_especial');
-            Route::post('/filtrado', [SoporteEspcialController::class, 'filtrado_soporte_especial_tecnico'])->name('soporte.filtrado.tec');
-    
-            Route::get('/agregar', [SoporteEspcialController::class, 'agregar_soporte_especial'])->name('sop.agregar_soporte_especial');
-            Route::get('/editar/{soporte}', [SoporteEspcialController::class, 'editar_soporte_especial'])->name('sop.editar_soporte_especial');
-            Route::post('/registrar', [SoporteEspcialController::class, 'registrar_soporte_especial'])->name('sop.registrar_soporte_especial');
-            Route::put('/actualizar/{soporte}', [SoporteEspcialController::class, 'actualizar_soporte_especial'])->name('sop.actualizar_soporte_especial');
-            Route::post('/registrar-actividad/{soporte}', [SoporteEspcialController::class, 'registrar_actividad_soporte'])->name('sop.registrar_actividad_soporte');
 
-            /* Revisor */
-            Route::get('/listado', [SoporteEspcialController::class, 'revisor_listar_soporte_especial'])->name('soporte.revisor_listar_soporte_especial');
-            Route::post('/filtrado', [SoporteEspcialController::class, 'filtrado_soporte_especial'])->name('soporte.filtrado_soporte_especial');
-
+            /* supervisor */
             Route::get('/listado', [SoporteEspcialController::class, 'supervisor_listar_soporte'])->name('especiales.listado_supervisor');
             Route::post('/filtrado-supervisor', [SoporteEspcialController::class, 'filtrado_supervisor_soporte'])->name('especiales.filtrado_supervidor');
         });
