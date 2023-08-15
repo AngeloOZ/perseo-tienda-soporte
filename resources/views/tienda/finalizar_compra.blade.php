60c39962451f068c1584b6d36527919813f055b1
angelooz <angelo-mjz7@hotmail.com>
2023-08-15 08:35:36 -0500

    chore: se elimino ;

--- resources/views/tienda/finalizar_compra.blade.php
+++ resources/views/tienda/finalizar_compra.blade.php
@@ -76,7 +76,7 @@
                                 </div>
                             </div>
                             <div class="col-12 col-lg-6">
-                                @include('tienda.datos_compra');
+                                @include('tienda.datos_compra')
                             </div>
                         </div>
                     </div>
