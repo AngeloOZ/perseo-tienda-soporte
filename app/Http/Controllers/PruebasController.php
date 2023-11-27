1619912588b4dd538fe7fbec7338031d7e18b385
AngelloOZ <angelo-mjz7@hotmail.com>
2023-11-27 09:18:55 -0500

    fix: convertir pdf

--- app/Http/Controllers/PruebasController.php
+++ app/Http/Controllers/PruebasController.php
@@ -8,6 +8,25 @@ class PruebasController extends Controller
 {
     public function word_to_pdf_python()
     {
-        
+        // Comando para ejecutar el script de Python
+        $python = 'python';
+        $BasePath = base_path();
+
+        // Path files
+        $pathInput = "public/word/pcControl.docx";
+        $pathOutput = "public/word";
+
+        // Comando para ejecutar el script de Python
+        $command = escapeshellcmd("$python $BasePath/scripts/convert_to_pdf.py $pathInput $pathOutput");
+
+        // Ejecuta el comando y captura la salida
+        $output = shell_exec($command . ' 2>&1');
+
+        if (str_contains($output, 'Error')) {
+            echo $output . "<br><br/>";
+            echo "Error al convertir el archivo";
+        } else {
+            echo "Archivo convertido correctamente";
+        }
     }
 }
--- scripts/convert_to_pdf.py
+++ scripts/convert_to_pdf.py
@@ -1,7 +1,26 @@
 import subprocess
+import sys
 
 def convert_doc_to_pdf(input_file, output_file):
     cmd = f"libreoffice --headless --convert-to pdf:writer_pdf_Export --outdir {output_file} {input_file}"
-    subprocess.run(cmd, shell=True)
 
-convert_doc_to_pdf("input.docx", "/path/to/output/folder")
+    try:
+        result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return result.stdout.decode(), None
+    except subprocess.CalledProcessError as e:
+        return None, e.stderr.decode()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Uso: script.py [input_file] [output_folder]")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_folder = sys.argv[2]
+
+    success, error = convert_doc_to_pdf(input_file, output_folder)
+
+    if error:
+        print(f"Error: {error}")
+    else:
+        print(f"Success: {success}")
